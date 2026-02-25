93b9e945e13eb620ea3e8dd17509e5fb6f95ce8c
Claudiu BĂRBIERU <claudiu.barbieru@gmail.com>
2026-02-25 22:44:31 +0200

    updated state

--- scripts/setup-metrics.sh
+++ scripts/setup-metrics.sh
@@ -11,7 +11,6 @@ fi
 DIR_PATH=$(dirname "$OUTPUT_FILE")
 mkdir -p "$DIR_PATH"
 
-# 3. Scriem antetul DOAR dacă fișierul nu există deja (ca să nu stricăm append-ul la restart)
 if [ ! -f "$OUTPUT_FILE" ]; then
     echo "Timestamp,Namespace,Pod,CPU(m),RAM(Mi)" > "$OUTPUT_FILE"
     echo "S-a creat fișierul nou: $OUTPUT_FILE"
@@ -21,18 +20,13 @@ fi
 
 echo "Start monitorizare... (Ctrl+C pentru stop)"
 
-# 4. Bucla infinită
 while true; do
     TS=$(date +'%Y-%m-%d %H:%M:%S')
     
-    # Colectare date
-    # sed 's/m//g' -> șterge "m"-ul de la millicores
-    # sed 's/Mi//g' -> șterge "Mi"-ul de la memorie
     kubectl top pods -A --no-headers | \
     sed 's/m//g' | sed 's/Mi//g' | \
     awk -v timestamp="$TS" '{print timestamp "," $1 "," $2 "," $3 "," $4}' >> "$OUTPUT_FILE"
     
-    # Feedback vizual minim (ca să știi că rulează)
     echo -n "."
     
     sleep 10
